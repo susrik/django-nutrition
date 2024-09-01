018a24a15de5211a29dd6c6ce9ef8f28e04baa22
Erik Reid <erik.reid@geant.org>
2024-09-01 19:37:10 +0200

    added __str__ methods on model tables

--- nutrition/models.py
+++ nutrition/models.py
@@ -6,14 +6,23 @@ class Food(models.Model):
     name = models.CharField(max_length=200)
     calories = models.FloatField()
 
+    def __str__(self):
+        return self.name
+
 
 class Meal(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Portion(models.Model):
     date = models.DateField(default=timezone.now().date())
-    quantity = models.IntegerField(default=1)
+    quantity = models.FloatField(default=1)
     note = models.CharField(max_length=200, blank=True)
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return f'{self.food.name} ({self.quantity})'
