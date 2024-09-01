60e7b0216ac13a1769b09f5ec6080e07a895b303
Erik Reid <erik.reid@geant.org>
2024-09-01 19:35:05 +0200

    registered models

--- nutrition/admin.py
+++ nutrition/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Food, Meal, Portion
 
-# Register your models here.
+admin.site.register(Food)
+admin.site.register(Meal)
+admin.site.register(Portion)
