0ef63d75a91e8f28effb45a7293e20135bc8875f
Erik Reid <erik.reid@geant.org>
2024-09-01 19:29:12 +0200

    initial version models, first migration

--- fitness/settings.py
+++ fitness/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nutrition.apps.NutritionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- nutrition/models.py
+++ nutrition/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    calories = models.FloatField()
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Portion(models.Model):
+    date = models.DateField(default=timezone.now().date())
+    quantity = models.IntegerField(default=1)
+    note = models.CharField(max_length=200, blank=True)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, blank=True)
