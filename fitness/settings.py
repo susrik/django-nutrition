ccca2a3a47f2057c038e6af8e15615d1fc40cac0
Erik Reid <erik.reid@geant.org>
2024-09-01 19:08:39 +0200

    update db creds

--- fitness/settings.py
+++ fitness/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,12 +74,24 @@ WSGI_APPLICATION = 'fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',  # Use SQLite for testing
+            'NAME': ':memory:',  # In-memory database for faster tests
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'NAME': 'nutrition',
+            'USER': 'nutrition-admin',
+            'PASSWORD': 'nutrition-secret',
+        },
+    }
+
 
 
 # Password validation
