b49a44dadd8cc0f12d7fc3c36fa73524c0e66726
Erik Reid <erik.reid@geant.org>
2024-09-02 13:50:07 +0200

    added some columns to portion admin ui

--- nutrition/admin.py
+++ nutrition/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Food, Meal, Portion
 
+
+class PortionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'food', 'quantity', 'meal')
+    list_filter = ['date']
+
+
 admin.site.register(Food)
 admin.site.register(Meal)
-admin.site.register(Portion)
+admin.site.register(Portion, PortionAdmin)
