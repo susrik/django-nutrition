2a1e50d5f3aec6c68ee858872af362c644e8e3fe
Erik Reid <erik.reid@geant.org>
2024-10-16 09:15:20 +0200

    put explicit style keywords in one class

--- django_nutrition/api.py
+++ django_nutrition/api.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from functools import partial
 from datetime import datetime
 from typing import Iterable, List, TYPE_CHECKING
 from django.utils import timezone
@@ -11,23 +12,13 @@ from rest_framework import serializers
 if TYPE_CHECKING:
     from django.http import HttpRequest
 
-OVER_THRESHOLD = .1  # cutoff for "slightly over"
+CALORIES_WARNING_THRESHOLD = .1  # cutoff for "slightly over"
+
 
 class DailyTotalRange(Enum):
     UNDER = "under"
     OVER = "over"
     SLIGHTLY_OVER = "slightly_over"
-
-
-def _get_calorie_range(calories: float, max_calories: float) -> DailyTotalRange:
-    if calories < max_calories:
-        return DailyTotalRange.UNDER
-    elif calories > max_calories * (1 + OVER_THRESHOLD):
-        return DailyTotalRange.OVER
-    else:
-        return DailyTotalRange.SLIGHTLY_OVER
-
-
 
 
 class MealTotal:
@@ -65,9 +56,38 @@ class DayTotal:
 
 class FullDayEvent:
 
+    CALORIE_RANGE_STYLES = {
+        DailyTotalRange.UNDER: {
+            "backgroundColor": "green",
+            "textColor": "white",
+        },
+        DailyTotalRange.OVER: {
+            "backgroundColor": "red",
+            "textColor": "white",
+        },
+        DailyTotalRange.SLIGHTLY_OVER: {
+            "backgroundColor": "orange",
+            "textColor": "black",
+        },
+    }
+
+    @staticmethod
+    def get_calorie_range(
+            calories: float,
+            max_calories: float,
+            warning_threshold: float = 0) -> DailyTotalRange:
+        assert warning_threshold > 0
+        if calories < max_calories:
+            return DailyTotalRange.UNDER
+        elif calories > max_calories * (1 + CALORIES_WARNING_THRESHOLD):
+            return DailyTotalRange.OVER
+        else:
+            return DailyTotalRange.SLIGHTLY_OVER
+
     def __init__(self,
                  day: DayTotal,
-                 range: DailyTotalRange = DailyTotalRange.UNDER):
+                 max_calories: float,
+                 warning_threshold: float = 0):
         self.title = str(int(day.calories + 0.5))
         self.start = day.date
         self.end = day.date
@@ -77,7 +97,8 @@ class FullDayEvent:
         self.display = "auto"
         self.url = f"/nutrition/day/{day.date}"
 
-        style = CALORIE_RANGE_STYLES[range]
+        range = self.get_calorie_range(day.calories, max_calories, warning_threshold)
+        style = FullDayEvent.CALORIE_RANGE_STYLES[range]
         self.backgroundColor = style["backgroundColor"]
         self.textColor = style["textColor"]
 
@@ -119,9 +140,8 @@ def days(request: "HttpRequest"):
 
     _prefs = models.Preferences.current_preferences(request)
 
-    def _make_event(day: DayTotal):
-        range = _get_calorie_range(day.calories, _prefs["max_calories"])
-        return FullDayEvent(day, range=range)
-
+    _make_event = partial(FullDayEvent,
+            max_calories=_prefs["max_calories"],
+            warning_threshold=CALORIES_WARNING_THRESHOLD)
     serializer = FullDayEventSerializer(map(_make_event, _days), many=True)
     return Response(serializer.data)
